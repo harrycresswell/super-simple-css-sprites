5b49e047439d41a7fc62fd02a28158b51ccca637
Harry Cresswell <studio@harrycresswell.com>
2018-02-13 16:20:28 +0000

    added image compression, minification and image path options

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 // Load dependencies
 var gulp = require('gulp');
+
+var buffer = require('vinyl-buffer');
+var csso = require('gulp-csso');
+var imagemin = require('gulp-imagemin');
+var merge = require('merge-stream');
+
 var spritesmith = require('gulp.spritesmith');
 
-
 gulp.task('sprite', function () {
+
   // Grab all normal and `@2x` (retina) images from `src
   //   e.g. `github.png`, `github@2x.png`
   var spriteData = gulp.src('./src/*.png').pipe(spritesmith({
@@ -12,13 +18,32 @@ gulp.task('sprite', function () {
       //   e.g. `github@2x.png`, `twitter@2x.png`
       retinaSrcFilter: './src/*@2x.png',
 
+      // set the background image urls to reference in CSS
+      imgPath: 'sprite.png',
+      retinaImgPath: 'sprite@2x.png',
+
       // Generate a normal and a `@2x` (retina) spritesheet
-      imgName: 'spritesheet.png',
-      retinaImgName: 'spritesheet@2x.png',
+      imgName: 'sprite.png',
+      retinaImgName: 'sprite@2x.png',
 
       // Generate CSS for both spritesheets
       cssName: 'sprites.css'
     }));
-    // Pipe CSS and spritesheets to `dist` folder
-   return spriteData.pipe(gulp.dest('./dist/'));
+
+    // Pipe image stream through image optimizer and onto disk
+ var imgStream = spriteData.img
+
+   // DEV: We must buffer our stream into a Buffer for `imagemin`
+   .pipe(buffer())
+   .pipe(imagemin())
+   .pipe(gulp.dest('./dist/'));
+
+ // Pipe CSS stream through CSS optimizer and onto disk
+ var cssStream = spriteData.css
+   .pipe(csso())
+   // Pipe CSS and spritesheets to `dist` folder
+   .pipe(gulp.dest('./dist/'));
+
+ // Return a merged stream to handle both `end` events
+ return merge(imgStream, cssStream);
 });
---
+++ todo.md
@@ -0,0 +1,3 @@
+#todo
+
+- Add [layout](https://github.com/Ensighten/grunt-spritesmith/issues/102) support to maintain original sort order
